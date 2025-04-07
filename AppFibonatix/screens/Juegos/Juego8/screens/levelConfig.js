71049850eb17bc4fadca524fcd8dbf3c366f9b11
Hector2809 <arellano.reyes.hector28@gmail.com>
2025-04-07 00:33:09 -0600

    Cambios a juegos 7 y 8

--- AppFibonatix/screens/Juegos/Juego8/screens/levelConfig.js
+++ AppFibonatix/screens/Juegos/Juego8/screens/levelConfig.js
@@ -1,27 +1,27 @@
 // levelConfig.js
 export const LEVEL_CONFIG = {
-    1: {
-      title: "DIBUJI TORTUGA",
-      instruction: "Encuentra las frutas",
-      words: ["MANZANA", "PLATANO", "PERA", "UVA", "MANGO", "KIWI", "NARANJA"],
-      gridSize: 10
-    },
-    2: {
-      title: "MULTIPLI TORTUGA",
-      instruction: "Encuentra las verduras",
-      words: ["LECHUGA", "JITOMATE", "PAPA", "CEBOLLA", "ELOTE", "RABANO", "CALABAZA", "CHAYOTE"],
-      gridSize: 11
-    },
-    3: {
-      title: "SUMI TORTUGA",
-      instruction: "Encuentra los animales",
-      words: ["PERRO", "GATO", "CONEJO", "LEON", "TIGRE", "ELEFANTE", "JIRAFA", "CEBRA", "MONO"],
-      gridSize: 12
-    },
-    4: {
-      title: "DIVI TORTUGA",
-      instruction: "Encuentra los colores",
-      words: ["ROJO", "AZUL", "VERDE", "AMARILLO", "MORADO", "NARANJA", "BLANCO", "NEGRO", "ROSA"],
-      gridSize: 12
-    }
-  };
+  1: {
+    title: "SOPA OPERATIVA",
+    instruction: "Encuentra las palabras",
+    words: ["SUMA", "TRES", "CUADRADO", "CINCO", "SIETE", "NUMERO", "DIEZ"],
+    gridSize: 10
+  },
+  2: {
+    title: "SOPA OPERATIVA",
+    instruction: "Encuentra las palabras",
+    words: ["QUINCE", "CIRCULO", "RESTA", "FRACCION", "AREA", "OCHO", "FIGURA", "TRECE"],
+    gridSize: 11
+  },
+  3: {
+    title: "SOPA OPERATIVA",
+    instruction: "Encuentra las palabras",
+    words: ["DECIMAL", "MULTIPLICACION", "OPERACION", "PERIMETRO", "RECTA", "ONCE", "TRIANGULO", "ROMBO", "CUATRO"],
+    gridSize: 12
+  },
+  4: {
+    title: "SOPA OPERATIVA",
+    instruction: "Encuentra las palabras",
+    words: ["VEINTE", "TRIANGULO", "ROMBOIDE", "DIVISION", "RECTANGULO", "TREINTA", "TRAPECIO", "ANGULO", "CUARENTA"],
+    gridSize: 12
+  }
+};
